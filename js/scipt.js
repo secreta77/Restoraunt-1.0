d452426fef17b1f476cb7e4a479489464150ab92
secreta77 <qoqashviligiorgi7@gmail.com>
2026-09-21 19:31:41 +0400

    fix header and add user me feature

---
+++ js/scipt.js
@@ -0,0 +1,33 @@
+const accessToken = localStorage.getItem('accessToken')
+
+if (accessToken) {
+    document.getElementById('userMenu').hidden = false
+    fetch(`${API_BASE_URL}/api/users/me`,{
+        headers:{
+            'X-API-KEY':API_KEY,
+            'Authorization': `Bearer ${accessToken}`
+        }
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(result){
+        document.getElementById('userNameLabel').textContent=result.data.firstName +' '+ result.data.lastName;
+    })
+
+
+} else {
+    document.getElementById('authButtons').hidden = false
+}
+
+document.getElementById('avatarBtn').addEventListener('click', function () {
+    const dropDown = document.getElementById('userDropdown')
+    dropDown.hidden = !dropDown.hidden
+
+})
+
+document.getElementById('logoutBtn').addEventListener('click',function(){
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    window.location.reload()
+})
